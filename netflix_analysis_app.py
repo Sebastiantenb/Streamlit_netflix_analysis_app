4c727429179a1f9488931a7f55545bde45e2c106
Sebastiantb <30553150+Sebastiantenb@users.noreply.github.com>
2022-08-21 13:47:27 +0200

    TMDB API implementation
    Implemented the tmdb_api and added various dependent visualisations on movie ratings and genres

--- netflix_analysis_app.py
+++ netflix_analysis_app.py
@@ -8,24 +8,35 @@ from matplotlib.colors import LinearSegmentedColormap
 import seaborn as sns
 from PIL import Image
 
+# The movie database api required packages
+
+import urllib.request
+import urllib.parse
+import json
+import requests
+
 
 # APP UI MODULES
 ##########################################################################################
 
 #title
-st.title('Netflix Analysis GDPR')  #Title of the webapp
+st.title('Netflix Profile Analysis')  #Title of the webapp
 
 #tmdb logo 
 tmdb_logo = Image.open('tmdb_logo.jpeg')
 st.image(tmdb_logo, caption='Powered by')
 
 #text
-st.markdown('Every online service that you use will store a surprising amount of data about you. \
- This is my hobby project to make your personal data on the internet truly accessible to you!\
-Netflix is one of the companies who give you easy access to your own data. You can download your own data here: **_netflix.com/youraccount_**')
+st.markdown('Every online service which you use will store a surprising amount of data about you. \
+ This is my hobby project to make your personal data more accessible and understandable to you!\
+Netflix is one of the companies who make your data easily accessible to you. \
+    [Download your Netflix Data here](netflix.com/youraccount)', unsafe_allow_html=False)
 
-st.markdown('This Project is work in progress. Next goals are to integrate it with The Movie DB to get more exiting insights such as favorite movie genres and movie ratings.\
-    If you have any suggestions on what to add to the analysis or want to talk about this project, feel free to reach out to me on **_linkedin.com/in/sebastian-ten-berge/_**')
+with st.expander('Want to know more? Or Do you have any questions or feedback?'):
+    st.markdown('I hope you enjoyed the insights to your Netflix Data :blush:.\
+            Let me know if there is anything else you would like to know about your Netflix data.\
+            I am happy about any suggestions, feedback, or just to talk about this project or anything Data. \
+            Feel free to reach out to me on [linkedin.com/in/sebastian-ten-berge](inkedin.com/in/sebastian-ten-berge)')
 
 ## !!! next goal is to create option to upload own netflix file. 
 
@@ -41,8 +52,9 @@ streaming_locations_df = pd.read_csv('Files/IpAddressesStreaming.csv')
 uploaded_file = st.sidebar.file_uploader("upload file", type="zip")
 
 st.sidebar.markdown('**What happens to my netflix data if I upload it?** :thinking_face:')
-st.sidebar.markdown('In short, files are stored in memory, they get deleted immediately as soon as they’re not needed anymore. \
-        You can find more on streamlits own documentation:\
+with st.sidebar.expander('Explanation'):
+     st.markdown('In short, files are stored in memory, they get deleted immediately as soon as they’re not needed anymore. \
+        You can find more in depth information in streamlits own documentation:\
          [where-file-uploader-store-when-deleted](https://docs.streamlit.io/knowledge-base/using-streamlit/where-file-uploader-store-when-deleted)', unsafe_allow_html=False )
 
 
@@ -104,7 +116,7 @@ total_watchtime_df =  watched_df.groupby(['Profile_Name'],as_index=False).sum().
 fig3 = px.bar(total_watchtime_df, x= 'Profile_Name',  y="watched_hours",
              color="watched_hours",
              color_continuous_scale=["rgb(1,1,1)", "rgb(86,77,77)", "rgb(131,16,16)", "rgb(219,0,0)"],
-             title="Hours Spent Watching Netflix")
+             title="Did we spend too much time watching Netflix?")
 st.plotly_chart(fig3, use_container_width=True)
 
 
@@ -141,7 +153,7 @@ if film_type_radio_button_1 == 'Movie':
                          y = 'Count', 
                          color = 'Profile_Name',
                          color_discrete_sequence=["rgb(1,1,1)", "rgb(219,0,0)", "rgb(86,77,77)", "rgb(131,16,16)"],
-                         title="Movies"
+                         title="Our Favorite Movies"
                          )
     fig5.update_layout(xaxis={'categoryorder':'total descending'})
     st.plotly_chart(fig5, use_container_width=True)
@@ -157,7 +169,7 @@ if film_type_radio_button_1 == 'Series':
                         y = 'Count', 
                         color = 'Profile_Name',
                         color_discrete_sequence=["rgb(1,1,1)", "rgb(219,0,0)", "rgb(86,77,77)", "rgb(131,16,16)"],
-                        title="Series")
+                        title="Our Favorite Series")
     fig6.update_layout(xaxis={'categoryorder':'total descending'})
     st.plotly_chart(fig6, use_container_width=True)
 
@@ -179,7 +191,7 @@ fig = px.choropleth(streaming_country_df,                            # Input Dat
                          projection= 'natural earth',        # select projection
                          range_color=[0,streaming_country_df['Start_Time'].max()],
                          labels={'Start_Time':'Hours Series/Movies Watched'},
-                         title= 'Countries viewed from')
+                         title= 'In which Countries have we watched Netflix?')
 st.plotly_chart(fig)
 fig.write_html("example_map.html")
 
@@ -208,7 +220,7 @@ df_devices_count = df_devices_count.drop(['sub_device'], axis = 1)
 df_devices_count = df_devices_count.groupby(['Device']).sum(['Count']).reset_index().sort_values(['Count'])
 
 fig4 = px.bar(df_devices_count, x= 'Device',  y="Count",
-             title="Most Popular Device to Watch from",
+             title="On which Device do we like to watch Netflix?",
              color='Count',
              color_continuous_scale=["rgb(1,1,1)", "rgb(86,77,77)", "rgb(131,16,16)", "rgb(219,0,0)"])
 st.plotly_chart(fig4, use_container_width=True)
@@ -255,9 +267,142 @@ fig2, ax = plt.subplots(figsize=(15, 10))
 black_red = LinearSegmentedColormap.from_list('black_red', ['white', 'darkgrey', 'darkred', 'red'])
 sns.heatmap(heatmap_df[heatmap_df.index.get_level_values('Profile_Name').isin([user_radio_button_2])].droplevel(1, axis=0),
             annot=True, fmt='g', cmap=black_red, ax=ax)
-ax.set_title(user_radio_button_2 + "'s" + " Netflix Log in Times", fontsize=20)
+ax.set_title("When does " + user_radio_button_2  + " like to watch Netflix?", fontsize=20)
 ax.set_xlabel('Weekday', fontsize=15)  
 ax.set_ylabel('Time', fontsize=15) 
 # ax.set_yticklabels(range(0,24), rotation=0)
 st.pyplot(fig2)
+
+
+
+# The Movie Database API
+
+##########################################################################################
+
+
+@st.cache
+def json_of_url(url):
+    webURL = urllib.request.urlopen(url)
+    data = webURL.read()
+    encoding = webURL.info().get_content_charset('utf-8')
+    JSON_object = json.loads(data.decode(encoding))
+    return JSON_object
+@st.cache
+def get_genre_ids():
+    genres_JSON = json_of_url("https://api.themoviedb.org/3/genre/movie/list?api_key=" +  tmdb_api_key)
+    return genres_JSON
+
+genre_by_id = get_genre_ids()
+#list to dataframe
+df_genres = pd.DataFrame.from_dict(genre_by_id['genres'], orient='columns')
+df_genres.rename(columns = {'id':'genre_ids', 'name':'genre'}, inplace = True)
+genre_by_id = df_genres.set_index('genre_ids').to_dict()['genre']
+
+genre_by_id.update({10765: "Sci-Fi, Fantasy", 10763:"News", 10759: "Action, Adventure", 10764:"Reality",10768:'War, Politics', 10766:"Soap", 10762:"Kids",10767:"Talk"})
+genre_by_id[878] = "Sci-Fi"
+
+## Requesting Movies from TMDB
+@st.cache
+def get_movie_data(movie_title):
+    movies_JSONs = json_of_url(("https://api.themoviedb.org/3/search/movie?query={}&language=en-US&page=1&include_adult=false&api_key=" +  tmdb_api_key).format(urllib.parse.quote_plus(movie_title)))
+    return movies_JSONs
+
+movies_watched = list(watched_df[(watched_df["Film_Type"] == 'Movie') & (watched_df["percent_watched2"] > 85)]["Title"])
+# series_watched = list(watched_df[(watched_df["Film_Type"] == 'Series') & (watched_df["percent_watched2"] > 85)]["Show_Title"])
+
+deduplicated_movies_watched = list(set(movies_watched))
+
+nested_dict_movie_titles = {}
+for movie_title in deduplicated_movies_watched:
+    nested_dict_movie_titles[movie_title] = get_movie_data(movie_title)
+
+# print(dict_movie_titles.values['results'])
+valuesofdic = list(nested_dict_movie_titles.values())
+dict_movie_titles = []
+for value_of_dic in valuesofdic:
+    res = value_of_dic['results']
+    dict_movie_titles.extend(res)
+
+#creating dataframe from dict of movies
+df_movie_database = pd.DataFrame.from_dict(dict_movie_titles, orient='columns')
+
+# removing duplicate movies
+df_movie_database['is_duplicated'] = df_movie_database.sort_values(['original_title','popularity' ], ascending=False).\
+                            duplicated(subset = ['original_title'], keep='first')
+df_movie_database = df_movie_database[df_movie_database['is_duplicated'] == False]
+
+#cleaning genre_ids column
+df_movie_database["genre_ids"] = df_movie_database["genre_ids"].astype("string")
+df_movie_database["genre_ids"] = df_movie_database["genre_ids"].str.replace('[', '')
+df_movie_database["genre_ids"] = df_movie_database["genre_ids"].str.replace(']', '')
+
+#adding genre to movie
+dic = {r"\b{}\b".format(k): v for k, v in genre_by_id.items()}
+df_movie_database["genre_ids"] = df_movie_database["genre_ids"].replace(dic, regex=True)
+df_movie_database2 = df_movie_database[['genre_ids', 'original_title', 'vote_average', 'vote_count', 'release_date']]
+df_movie_database2[df_movie_database2['vote_count'] > 5000].sort_values(by=['vote_average', 'vote_count'], ascending=False)
+
+df_movies = watched_df[watched_df['Film_Type'] == 'Movie'].merge(df_movie_database2,how='left',left_on=['Show_Title'],right_on=['original_title'])
+
+# Pie Chart Movies
+##########################################################################################
+
+df_movies_watched = df_movies[df_movies['percent_watched2'] >= 80].dropna(subset=['genre_ids'])
+
+dict_movie_user_genres = {}
+for user in users:
+    dict_movie_user_genres[user] = " ".join(review for review in df_movies_watched[df_movies_watched['Profile_Name'] == user].genre_ids)
+
+for user in users:
+    dict_movie_user_genres[user] = dict_movie_user_genres[user].replace(",","")
+
+# function to count words
+@st.cache
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+
+movie_genres_by_user = {}
+for user in users:
+     movie_genres_by_user[user] = word_count(dict_movie_user_genres[user])
+
+df_movies_genre_frequency = pd.DataFrame.from_dict(movie_genres_by_user, orient='columns').reset_index().fillna(value=0)
+
+df_movies_genre_frequency2 = df_movies_genre_frequency.melt(id_vars=['index'], var_name='Profile_Name', value_name='Count')
+
+fig_pie_movie = px.pie(df_movies_genre_frequency2[df_movies_genre_frequency2['Profile_Name'].isin(user_radio_button_1)], 
+             values='Count', 
+             color='index',
+             color_discrete_sequence=["rgb(1,1,1)", "rgb(131,16,16)", "rgb(86,77,77)", "rgb(219,0,0)"],
+             title = 'Which Movie Genres do I watch the most?',
+             names='index')
+fig_pie_movie.update_traces(textposition='inside', textinfo='percent+label')
+st.plotly_chart(fig_pie_movie, use_container_width=True)
+
+
+# Average Movie rating
+##########################################################################################
+
+
+df_movies_ratings = df_movies_watched[['Profile_Name', 'genre_ids', 'vote_average']]
+df_movies_ratings= df_movies_ratings.replace(0,np.NaN)
+df_movies_ratings.rename(columns = {'genre_ids':'genre', 'vote_average':'Movie_Rating'}, inplace = True)
+
+fig_rating = px.violin(df_movies_ratings[df_movies_ratings['genre'].str.contains('Comedy')],
+                    x= 'Profile_Name',  y="Movie_Rating",
+                    color="Profile_Name",
+                    box=True,
+                    title= 'How are the Movies I watched Rated?',
+                    color_discrete_sequence=["rgb(1,1,1)", "rgb(86,77,77)", "rgb(131,16,16)", "rgb(219,0,0)"])
+
+st.plotly_chart(fig_rating, use_container_width=True)
 
